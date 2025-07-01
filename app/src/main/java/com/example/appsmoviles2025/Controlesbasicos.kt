6eb2453a56313dde9a34e6264f4195e962207f81
Pedro <salazar9_5@hotmail.com>
2025-06-30 18:27:29 -0700

    implemntacion en clase

--- app/src/main/java/com/example/appsmoviles2025/Controlesbasicos.kt
+++ app/src/main/java/com/example/appsmoviles2025/Controlesbasicos.kt
@@ -47,116 +47,116 @@ class ComtrolesBasicos : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun FullPage() {
-    Scaffold(modifier = Modifier.fillMaxSize().statusBarsPadding()) { innerPadding ->
-        Contendido(
-            modifier = Modifier.padding(innerPadding)
-        )
-    }
-}
-
-
-@Composable
-fun Contendido(modifier: Modifier = Modifier) {
-    TextTest()
-}
-
-@Composable
-fun TextTest() {
-    Text("Prueba de texto", modifier = Modifier.background(Color.Red))
-}
-@Composable
-fun ButtonTest() {
-Button(onClick = {},
-    colors = ButtonDefaults.buttonColors(
-    containerColor = Color.Red, // Color de fondo
-    contentColor = Color.White // Color del texto
-)) { Text("este es un boton") }
-}
-
-@Composable
-fun ButtonTestToast() {
-    val context = LocalContext.current
-
-    Button(
-        onClick = {
-            Toast.makeText(
-                context,
-                "¡Mensaje toast!",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-    ) {
-        Text("Mostrar Toast")
-    }
-}
-@Composable
-fun TextFieldTest(){
-    TextField(value = "", onValueChange = {})
-}
-@Composable
-fun ImageTest(){
-    Image(
-        painter = painterResource(R.drawable.trifuerza),
-        contentDescription = ""
-    )
-}
-
-@Composable
-fun ColumnTest(){
-    Column() {  }
-}
-@Composable
-fun BotonContador(){
-//    var contador by rememberSaveable  { mutableStateOf(0) }
-    var contador by remember { mutableStateOf(0) }
-    Column {
-        Button(onClick = {contador++}) { Text("+") }
-        Button(onClick = {contador--}) { Text("-") }
-        Text(contador.toString())
-    }
-
-}
-
-@Composable
-fun BotonSaludo() {
-    // Estado para el TextField
-// Estados corregidos
-    var name by remember { mutableStateOf("") }
-    var texto by remember { mutableStateOf("") }
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        // TextField para entrada de texto
-        TextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text("Ingresa tu nombre") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        // Botón que genera el saludo
-        Button(
-            onClick = {
-                texto = if (name.isNotBlank()) "Hola $name!" else "Hola desconocido!"
-            },
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
-            Text("Saludar")
-        }
-
-
-        // Texto que muestra el saludo
-        Text(
-            text = texto,
-            modifier = Modifier.background(Color.Red)
-        )
-    }
-}
-
+//
+//@Composable
+//fun FullPage() {
+//    Scaffold(modifier = Modifier.fillMaxSize().statusBarsPadding()) { innerPadding ->
+//        Contendido(
+//            modifier = Modifier.padding(innerPadding)
+//        )
+//    }
+//}
+//
+//
+//@Composable
+//fun Contendido(modifier: Modifier = Modifier) {
+//    TextTest()
+//}
+//
+//@Composable
+//fun TextTest() {
+//    Text("Prueba de texto", modifier = Modifier.background(Color.Red))
+//}
+//@Composable
+//fun ButtonTest() {
+//Button(onClick = {},
+//    colors = ButtonDefaults.buttonColors(
+//    containerColor = Color.Red, // Color de fondo
+//    contentColor = Color.White // Color del texto
+//)) { Text("este es un boton") }
+//}
+//
+//@Composable
+//fun ButtonTestToast() {
+//    val context = LocalContext.current
+//
+//    Button(
+//        onClick = {
+//            Toast.makeText(
+//                context,
+//                "¡Mensaje toast!",
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        }
+//    ) {
+//        Text("Mostrar Toast")
+//    }
+//}
+//@Composable
+//fun TextFieldTest(){
+//    TextField(value = "", onValueChange = {})
+//}
+//@Composable
+//fun ImageTest(){
+//    Image(
+//        painter = painterResource(R.drawable.trifuerza),
+//        contentDescription = ""
+//    )
+//}
+//
+//@Composable
+//fun ColumnTest(){
+//    Column() {  }
+//}
+//@Composable
+//fun BotonContador(){
+////    var contador by rememberSaveable  { mutableStateOf(0) }
+//    var contador by remember { mutableStateOf(0) }
+//    Column {
+//        Button(onClick = {contador++}) { Text("+") }
+//        Button(onClick = {contador--}) { Text("-") }
+//        Text(contador.toString())
+//    }
+//
+//}
+//
+//@Composable
+//fun BotonSaludo() {
+//    // Estado para el TextField
+//// Estados corregidos
+//    var name by remember { mutableStateOf("") }
+//    var texto by remember { mutableStateOf("") }
+//    Column(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(16.dp),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        verticalArrangement = Arrangement.spacedBy(16.dp)
+//    ) {
+//        // TextField para entrada de texto
+//        TextField(
+//            value = name,
+//            onValueChange = { name = it },
+//            label = { Text("Ingresa tu nombre") },
+//            modifier = Modifier.fillMaxWidth()
+//        )
+//
+//        // Botón que genera el saludo
+//        Button(
+//            onClick = {
+//                texto = if (name.isNotBlank()) "Hola $name!" else "Hola desconocido!"
+//            },
+//            modifier = Modifier.padding(top = 8.dp)
+//        ) {
+//            Text("Saludar")
+//        }
+//
+//
+//        // Texto que muestra el saludo
+//        Text(
+//            text = texto,
+//            modifier = Modifier.background(Color.Red)
+//        )
+//    }
+//}
+//
--- app/src/main/java/com/example/appsmoviles2025/MainActivity.kt
+++ app/src/main/java/com/example/appsmoviles2025/MainActivity.kt
@@ -4,15 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.appsmoviles2025.ui.theme.AppsMoviles2025Theme
@@ -23,28 +38,135 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppsMoviles2025Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting("sdfsdf",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                FullPage()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun FullPage() {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+    ) { innerPadding ->
+        Contendido(
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun Contendido(modifier: Modifier = Modifier) {
+    ColumnTest()
+}
+
+
+@Composable
+fun TextTest() {
+//    Text("Prueba de texto", modifier = Modifier.background(Color.Red))
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        "texto a mostrar de prueba",
+        color = Color.Red,
+        modifier = Modifier
+            .background(Color.Blue)
+            .padding(5.dp)
+    )
+
+}
+
+
+@Composable
+fun TextFieldTest() {
+    TextField(
+        value = "",
+        onValueChange = {},
+        modifier = Modifier.background(Color.Cyan)
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AppsMoviles2025Theme {
-        Greeting("Android")
+fun ButtonTest() {
+    Button(
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Red, // Color de fondo
+            contentColor = Color.White // Color del texto
+        )
+    ) { Text("este es un boton") }
+}
+
+@Composable
+fun ImageTest() {
+    Image(
+        painter = painterResource(R.drawable.trifuerza),
+        contentDescription = ""
+    )
+}
+
+@Composable
+fun Testing() {
+    Text("Texto de prueba")
+    Text("Texto de prueba 2")
+    Text("Texto de prueba 3")
+}
+
+@Composable
+fun ColumnTest(){
+    Column {
+        Text("Texto de prueba")
+        Text("Texto de prueba 2")
+        Text("Texto de prueba 3")
     }
 }
+
+@Composable
+fun RowTest(){
+    Row {
+        Text("Texto de prueba")
+        Text("Texto de prueba 2")
+        Text("Texto de prueba 3")
+    }
+}
+
+
+
+@Composable
+fun BotonSaludo() {
+    // Estado para el TextField
+// Estados corregidos
+    var name by remember { mutableStateOf("") }
+    var texto by remember { mutableStateOf("") }
+    Column(
+
+    ) {
+        // TextField para entrada de texto
+        TextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Ingresa tu nombre") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        // Botón que genera el saludo
+        Button(
+            onClick = {
+                texto =  "Hola $name!"
+            },
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text("Saludar")
+        }
+
+
+        // Texto que muestra el saludo
+        Text(
+            text = texto,
+            modifier = Modifier.background(Color.Red)
+        )
+    }
+}
+
+
+
