7807ad83c27fbf15f3297defb7ca8ac9e3c41082
Pedro <salazar9_5@hotmail.com>
2025-07-01 22:15:39 -0700

    listas

--- app/src/main/java/com/example/appsmoviles2025/MainActivity.kt
+++ app/src/main/java/com/example/appsmoviles2025/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.appsmoviles2025
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,6 +14,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
@@ -20,12 +23,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
@@ -59,7 +65,7 @@ fun FullPage() {
 
 @Composable
 fun Contendido(modifier: Modifier = Modifier) {
-    ColumnTest()
+    ListaNombres()
 }
 
 
@@ -113,7 +119,7 @@ fun Testing() {
 }
 
 @Composable
-fun ColumnTest(){
+fun ColumnTest() {
     Column {
         Text("Texto de prueba")
         Text("Texto de prueba 2")
@@ -122,14 +128,13 @@ fun ColumnTest(){
 }
 
 @Composable
-fun RowTest(){
+fun RowTest() {
     Row {
         Text("Texto de prueba")
         Text("Texto de prueba 2")
         Text("Texto de prueba 3")
     }
 }
-
 
 
 @Composable
@@ -152,7 +157,7 @@ fun BotonSaludo() {
         // Botón que genera el saludo
         Button(
             onClick = {
-                texto =  "Hola $name!"
+                texto = "Hola $name!"
             },
             modifier = Modifier.padding(top = 8.dp)
         ) {
@@ -167,6 +172,135 @@ fun BotonSaludo() {
         )
     }
 }
+
+@Composable
+fun BotonContador() {
+
+    var contador by rememberSaveable { mutableStateOf(0) }
+    Column {
+        Button(onClick = { contador++ }) { Text("+") }
+        Button(onClick = { contador-- }) { Text("-") }
+        Text(contador.toString())
+    }
+
+}
+
+@Composable
+fun ButtonTestToast() {
+    val context = LocalContext.current
+
+    Button(
+        onClick = {
+            Toast.makeText(
+                context,
+                "¡Mensaje toast!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    ) {
+        Text("Mostrar Toast")
+    }
+}
+
+@Composable
+fun ListasTest() {
+//    val arreglo = arrayOf("Ema", "Gael", "Chuy", "Mr. Abundis")
+//    arreglo[0] = "Emmanuel"
+//    Text(text = arreglo[0])
+//    val lista: List<String> = listOf("Ema", "Gael", "Chuy", "Mr. Abundis")
+    val lista = mutableListOf(
+        "Ema",
+        "Gael",
+        "Chuy",
+        "Javier",
+        "Mr. Abundis",
+        "Julian",
+        "Julius",
+        "Max",
+        "Max",
+        "Max",
+        "Max",
+        "Max",
+        "Max",
+        "Max",
+        "Max",
+        "Yat",
+        "Carlos",
+        "Miguel",
+        "Millan",
+        "Comalito",
+        "Toledo",
+        "Alfredo",
+        "Nelson",
+        "Corazon"
+    )
+    // Text(text = lista[3])
+    LazyColumn {
+        items(lista) { persona ->
+            Text(
+                text = persona,
+                modifier = Modifier.padding(10.dp)
+            )
+        }
+
+    }
+}
+
+
+@Composable
+fun ListaNombres() {
+    //mostrar un textfield y boton que tome el valor del textfield
+    var listanombres = rememberSaveable { mutableStateListOf<String>() }
+
+    var name by remember { mutableStateOf("") }
+    Column() {
+        TextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Ingresa tu nombre") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = {
+                listanombres.add(name)
+            },
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text("Aceptar")
+        }
+        LazyColumn {
+            items(listanombres) { persona ->
+                Text(
+                    text = persona,
+                    modifier = Modifier.padding(10.dp)
+                )
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
